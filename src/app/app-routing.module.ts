b6487da1c51d7b251802ef82dd3026e38b9bf466
Philip Burggraf <burggrafdev88@gmail.com>
2021-05-17 07:45:04 -0500

    5.17 - hello world bean service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {WelcomeComponent} from './welcome/welcome.component';
+import {MasterAgreementAddComponent} from './master-agreement-add/master-agreement-add.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'welcome',
+    component: WelcomeComponent
+  },
+  {
+    path: 'master-agreement-add',
+    component: MasterAgreementAddComponent
+  },
+  {
+    path: '',
+    redirectTo: '/welcome',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,33 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {FooterComponent} from './footer/footer.component';
 import {WelcomeComponent} from './welcome/welcome.component';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+import {MasterAgreementAddComponent} from './master-agreement-add/master-agreement-add.component';
+import { ListTodosComponent } from './list-todos/list-todos.component';
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        HeaderComponent,
-        FooterComponent,
-        WelcomeComponent
-    ],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    WelcomeComponent,
+    MasterAgreementAddComponent,
+    ListTodosComponent
+  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
